7cfa8aa499570950077ff2d24060c42f936511db
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-09-05 19:10:35 +0900

    [Gold V] Title: 암호 만들기, Time: 192 ms, Memory: 10980 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin')
+                .toString()
+                .trim()
+                .split('\n')
+                .map(elem => elem.split(' '));
+
+
+const [L, C] = input[0].map(Number);
+const arr = input[1];
+const visitedList = new Array(L).fill('')
+let res = [];
+
+const recursion = (current, start) => {
+    if (current === L) {
+        const copied = [...visitedList];
+
+        const hasConsonant = copied.some(elem => 'aeiou'.includes(elem));
+        const hasVowel = copied.filter(elem => 'bcdfghjklmnpqrstvwxyz'.includes(elem)).length > 1;
+        if (hasConsonant && hasVowel) {
+            copied.sort();
+            res.push(copied.join(''));
+        }
+
+        return;
+    }
+    for (let i = start; i < C; i++) {
+        if (visitedList[current] == '') {
+            visitedList[current] = arr[i];
+            recursion(current + 1, i + 1);
+            visitedList[current] = '';
+        }
+    }
+}
+
+recursion(0, 0);
+res.sort()
+console.log(res.join('\n'));
+
+
+
