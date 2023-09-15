cda651857377ee0d6726592acd0b4e5c5e736571
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-09-15 20:17:19 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 188 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+// const input = +fs.readFileSync('/dev/stdin').toString();
+const input = fs.readFileSync('/dev/stdin')
+                    .toString()
+                    .trim()
+                    .split('\n')
+                    .map(elem => elem.split(' ').map(Number));
+
+
+
+const [arr] = input.slice(1);
+const dp = arr.map(elem => 1);
+
+
+for (let i = 1; i < arr.length; i++) {
+    for (let j = i - 1; j >=0; j--) {
+        if(arr[i] < arr[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+}
+
+console.log(Math.max(...dp));
