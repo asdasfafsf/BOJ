9a0be909378530171dbe4abf3782cdbe0aa4c7d0
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-09-22 18:53:54 +0900

    [Bronze V] Title: A-B, Time: 148 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt' )
+                    .toString()
+                    .trim();
+
+
+
+
+const [A, B] = input.split(' ').map(Number);
+console.log(A - B);
