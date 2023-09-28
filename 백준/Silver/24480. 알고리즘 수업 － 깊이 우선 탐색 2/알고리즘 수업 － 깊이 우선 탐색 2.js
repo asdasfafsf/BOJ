9d5eb87c402eabb8aacdad7a4e945ec714c07deb
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-09-28 16:03:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 792 ms, Memory: 105056 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt' )
+                    .toString()
+                    .trim()
+                    .split('\n')
+               
+
+
+const [N, M, R] = input[0].split(' ').map(Number);
+const arr = input.slice(1)
+                .map(elem => elem.split(' ').map(Number));
+
+const graph = Array.from({length: N + 1})
+                .map(elem => []);
+
+
+arr.forEach(([start, end]) => {
+    graph[start].push(end);
+    graph[end].push(start);
+})
+
+graph.forEach(elem => elem.sort((a, b) => b - a));
+const visited = new Array(N + 1).fill(false);
+
+
+let count = 0;
+let answer = new Array(N).fill(0);
+const dfs = (R) => {
+    visited[R] = true;
+    answer[R - 1] = ++count;
+
+    for (const node of graph[R]) {
+        if (visited[node] === false) {
+            dfs(node);
+        }
+    }
+}
+
+
+dfs(R);
+
+console.log(answer.join('\n'));
