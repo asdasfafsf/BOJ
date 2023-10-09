f5324d221b07d6181f944f94fe12adbb973690b2
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2023-10-09 18:08:06 +0900

    [Silver II] Title: 최대 힙, Time: 280 ms, Memory: 29948 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.js
@@ -0,0 +1,99 @@
+class ProrityQueue {
+
+    constructor() {
+        this.heap = [null];
+    }
+
+    swap = (i, j) => {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+
+    push = (data) => {
+        this.heap.push(data);
+
+        let cur = this.heap.length - 1; 
+        let par = Math.floor(cur / 2);
+
+        while(cur > 1 && this.heap[cur] > this.heap[par]) {
+            this.swap(cur, par);
+            cur = par;
+            par = Math.floor(cur / 2);
+        }
+    }
+
+    pop = () => {
+        const root = this.heap[1];
+
+        if (this.heap.length <= 2) {
+            this.heap = [null];
+            return root;
+        }
+
+        let cur = 1;
+        let left = cur * 2;
+        let right = cur * 2 + 1;
+
+        this.heap[1] = this.heap.pop();
+
+        while( (left < this.heap.length && this.heap[left] > this.heap[cur])
+             || (right < this.heap.length && this.heap[right] > this.heap[cur])) {
+
+            let target = left
+
+            if (this.heap[left] && this.heap[right]) {
+                target = this.heap[left] > this.heap[right] ? left : right;
+            }
+
+            this.swap(target, cur);
+            cur = target;
+
+            left = cur * 2;
+            right = cur * 2 + 1;
+        }
+
+
+        return root;
+    }
+
+
+    isEmpty = () => {
+        return this.heap.length === 1;
+    }
+
+    isFull = () => {
+        return !this.isEmpty()
+    }
+
+    peek = () => {
+        return this.heap.at(-1);
+    }
+}
+
+
+const fs = require('fs')
+const [N, ...arr] = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt' )
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number)
+
+
+const queue = new ProrityQueue();
+const answer = [];
+
+for (let i = 0; i < arr.length; i++) {
+    const r = arr[i]
+
+    if (r === 0) {
+        if (queue.isEmpty()) {
+            answer.push(0)
+        } else {
+            const pop = queue.pop();
+            answer.push(pop == null ? 0 : pop);
+        }
+    } else {
+        queue.push(r);
+    }
+}
+
+console.log(answer.join('\n'));
