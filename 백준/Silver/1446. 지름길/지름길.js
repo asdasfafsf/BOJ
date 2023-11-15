a5734b6ee288a26d73ef361335a85df154a9b794
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2023-11-15 21:04:26 +0900

    [Silver I] Title: 지름길, Time: 116 ms, Memory: 9444 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.js
+++ 백준/Silver/1446. 지름길/지름길.js
@@ -1,73 +1,3 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [null];
-    }
-
-    compare(i, j) {
-        const a = this.heap[i];
-        const b = this.heap[j];
-    }
-
-    swap(i, j) {
-        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-    }
-    enqueue(data) {
-        this.heap.push(data);
-        let cur = this.heap.length - 1;
-        let par = Math.floor(cur / 2);
-
-        while (cur > 1 && this.compare(cur, par)) {
-            this.swap(cur, par);
-            cur = par;
-            par = Math.floor(cur / 2);
-        }   
-    }
-
-    dequeue() {
-        const root = this.heap[1];
-
-        if (this.heap.length < 2) {
-            this.heap = [null];
-            return root;
-        }
-
-        this.heap[1] = this.heap.pop();
-        
-        let par = 1;
-        let left = par * 2;
-        let right = par * 2 + 1;
-
-        while (
-            (left < this.heap.length && this.compare(left, par))
-            || (right < this.heap.length && this.compare(right, par))
-        ) {
-            let target = left;
-
-            if (this.heap[left] && this.heap[right]) {
-                target = this.compare(left, right) ? left : right;
-            }
-
-            this.swap(target, cur);
-            cur = target;
-
-            left = cur * 2;
-            right = cur * 2 + 1;
-        }
-
-        return root;
-    }
-
-    isEmpty() {
-        return this.heap.length === 1;
-    }
-
-    isFull() {
-        return this.heap.length > 1;
-    }
-}
-
-
-
 const fs = require('fs')
 const input = fs.readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt' )
     .toString()
@@ -102,4 +32,3 @@ for (let i = 1; i <= D; i++) {
 }
 
 console.log(dp.at(-1))
-
