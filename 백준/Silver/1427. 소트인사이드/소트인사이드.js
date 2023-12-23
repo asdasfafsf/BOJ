d909b56cf02aa1a47febf3c505561b593505970f
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2023-12-23 15:24:01 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,9 @@
+const answer = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split('')
+    .sort((a, b) => b - a)
+    .join('');
+
+console.log(answer);
