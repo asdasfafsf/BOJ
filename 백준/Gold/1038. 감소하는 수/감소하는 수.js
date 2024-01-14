4c5db935394bb18b566419ad9db429d162744384
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-01-14 16:30:35 +0900

    [Gold V] Title: 감소하는 수, Time: 120 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.js
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.js
@@ -6,10 +6,8 @@ const input = require('fs')
 
 const N = +input;
 
-let count = 9;
 const queue = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let current = 1;
-let answer = -1;
 
 while (queue.length !== current) {
     const data = queue[current];
@@ -17,10 +15,6 @@ while (queue.length !== current) {
 
     for (let i = 0; i < target; i++) {
         queue.push(`${data}${i}`);
-
-        if (count === N) {
-            answer = `${data}${i}`;
-        }
     }
 
     current++;
