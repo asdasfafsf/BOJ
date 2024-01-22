2f103a6a868a47290be135b70c4c9fac38b97512
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-01-22 19:43:23 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.js
@@ -0,0 +1,39 @@
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+
+const N = +input[0];
+const parents = input[1].split(' ').map(Number);
+const removed = +input[2];
+
+
+const graph = Array.from({length: N}, () => []);
+for (let i = 0; i < parents.length; i++) {
+    const parent = parents[i];
+
+    if (parent > -1 && i !== removed) {
+        graph[parent].push(i)
+    }
+}
+
+const queue = [removed];
+let current = 0;
+
+while(queue.length !== current) {
+    const node = queue[current];
+
+    for (const child of graph[node]) {
+        queue.push(child)
+    }
+
+    graph[node] = undefined;
+
+    current++;
+}
+
+const answer = graph.reduce((pv, cv) => pv + (cv?.length === 0 ? 1 : 0), 0)
+console.log(answer)
