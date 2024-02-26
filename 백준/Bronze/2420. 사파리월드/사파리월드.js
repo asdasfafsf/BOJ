a382de69ce511fab813a874d25c9377d13bc5dc6
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-02-26 19:28:37 +0900

    [Bronze V] Title: 사파리월드, Time: 136 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.js
@@ -0,0 +1,8 @@
+const [N, M] = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split(' ')
+    .map(Number);
+
+console.log(Math.abs(N - M));
