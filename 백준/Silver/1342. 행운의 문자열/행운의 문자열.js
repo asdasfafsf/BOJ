a2819bea215d7c3fe37f9ea7f534422e75b79592
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-02-29 20:39:16 +0900

    [Silver I] Title: 행운의 문자열, Time: 644 ms, Memory: 10336 KB -BaekjoonHub

---
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.js
@@ -0,0 +1,32 @@
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim();
+
+const alphabets = Array.from(Array(26), () => 0);
+
+for (let i = 0; i < input.length; i++) {
+    alphabets[input.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+}
+
+const target = [...new Set(input)].join('');
+let answer = 0;
+const recursion = (depth, last) => {
+    if (depth === input.length) {
+        answer++;
+        return;
+    }
+
+    for (let i = 0; i < target.length; i++) {
+        const cur = target.charCodeAt(i) - 'a'.charCodeAt(0);
+
+        if (alphabets[cur] > 0 && last !== cur) {
+            alphabets[cur]--;
+            recursion(depth + 1,cur)
+            alphabets[cur]++;
+        }
+    }
+}
+
+recursion(0, '');
+console.log(answer);
