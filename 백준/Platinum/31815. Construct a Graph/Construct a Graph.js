52029ef690e6509f189abf45ad2f38f11515ba7a
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-05-12 20:34:59 +0900

    [Platinum V] Title: Construct a Graph, Time: 480 ms, Memory: 29700 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/31815. Construct a Graph/Construct a Graph.js
+++ 백준/Platinum/31815. Construct a Graph/Construct a Graph.js
@@ -11,20 +11,21 @@ for (let i = 1; i <= N; i++) {
     D.push(input[i].split(' ').map(Number));
 }
 
-// 행렬 D가 유효한지 확인
-let isValid = true;
+let isOk = true;
 for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-        if (D[i][j] !== D[j][i] || (i !== j && D[i][j] <= 0)) {
-            isValid = false;
-            break;
+        if (i !== j && D[i][j] <= 0) {
+            isOk = false;
+        }
+        if (D[i][j] !== D[j][i]) {
+            isOk = false;
         }
     }
 }
 
-if (!isValid) {
+if (!isOk) {
     console.log(-1);
-    process.exit(0)
+    process.exit(0);
 }
 
 for (let k = 0; k < N; k++) {
@@ -38,23 +39,21 @@ for (let k = 0; k < N; k++) {
     }
 }
 
-const edges = [];
+const answer = [];
 for (let i = 0; i < N; i++) {
     for (let j = i + 1; j < N; j++) {
         let isDirectEdge = true;
         for (let k = 0; k < N; k++) {
-            if (k !== i && k !== j && D[i][j] === D[i][k] + D[k][j]) {
+            if (k !== i && k !== j && D[i][j] >= D[i][k] + D[k][j]) {
                 isDirectEdge = false;
                 break;
             }
         }
         if (isDirectEdge) {
-            edges.push([i + 1, j + 1, D[i][j]]);
+            answer.push([i + 1, j + 1, D[i][j]]);
         }
     }
 }
 
-console.log(edges.length);
-edges.forEach(edge => {
-    console.log(edge.join(' '));
-});
+console.log(answer.length);
+console.log(answer.map(elem => elem.join(' ')).join('\n'));
