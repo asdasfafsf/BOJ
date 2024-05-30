910112f59a2197bee4714cf5c5818b94c06bbc1e
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-05-30 19:43:14 +0900

    [Gold III] Title: 등수 찾기, Time: 1212 ms, Memory: 127968 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17616. 등수 찾기/등수 찾기.js
+++ 백준/Gold/17616. 등수 찾기/등수 찾기.js
@@ -7,86 +7,37 @@ const input = require('fs')
 const [N, M, X] = input[0].split(' ').map(Number);
 
 
-const graph = Array.from(Array(N + 1), () => []);
-const reversedGraph = Array.from(Array(N + 1), () => []);
-
-const ind = Array(N + 1).fill(0);
-const reversedInd = Array(N + 1).fill(0);
+const graph = Array.from(Array(N), () => []);
+const reversedGraph = Array.from(Array(N), () => []);
 
 
 for (let i = 1; i < input.length; i++) {
-    const [n1, n2] = input[i].split(' ').map(Number);
-
+    const [n1, n2] = input[i].split(' ').map(elem => elem - 1);
     graph[n1].push(n2);
-    ind[n2]++;
-
     reversedGraph[n2].push(n1);
-    reversedInd[n1]++;
-}
-
-const queue = [];
-const reversedQueue = [];
-
-for (let i = 1; i < ind.length; i++) {
-    if (ind[i] === 0) {
-        queue.push(i);
-    }
-
-    if (reversedInd[i] === 0) {
-        reversedQueue.push(i);
-    }
 }
 
 
-let current = 0;
-let isFind = false;
+let n = 0;
+const recursion = (start, graph, visited, depth) => {
+    n++;
+    visited[start] = true;
 
-let min = 1;
-while (queue.length !== current && !isFind) {
-    let currentIndex = queue.length;
-
-    const node = queue[current];
-
-    for (const nextNode of graph[node]) {
-        ind[nextNode]--;
-
-        if (ind[nextNode] === 0) {
-            if (nextNode === X) {
-                isFind = true;
-                min = currentIndex + 1;
-                break;
-            }
-            queue.push(nextNode);
+    for (const nextNode of graph[start]) {
+        if (visited[nextNode]) {
+            continue;
         }
+
+        recursion(nextNode, graph, visited);
     }
-    current++;
 }
 
+n = 0;
+recursion(X - 1, graph, Array(N).fill(false))
+const max = N - n + 1;
 
-current = 0;
-isFind = false;
+n = 0;
+recursion(X - 1, reversedGraph, Array(N).fill(false))
+const min = n;
 
-let max = 1;
-while (reversedQueue.length !== current && !isFind) {
-    let currentIndex = reversedQueue.length;
-
-    const node = reversedQueue[current];
-
-    for (const nextNode of reversedGraph[node]) {
-        reversedInd[nextNode]--;
-
-        if (reversedInd[nextNode] === 0) {
-            if (nextNode === X) {
-                isFind = true;
-                max = currentIndex + 1;
-                break;
-            }
-            reversedQueue.push(nextNode);
-        }
-    }
-    current++;
-}
-
-console.log(min , N - max + 1)
-
-
+console.log(min, max)
