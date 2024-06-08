f020c5e34a445f7a282002e7726e559df8a91383
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-06-08 15:51:39 +0900

    [Gold III] Title: 개구리 점프, Time: 1008 ms, Memory: 98592 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.js
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.js
@@ -9,63 +9,36 @@ const [N, Q] = input[0].split(' ').map(Number);
 
 const logs = [];
 for (let i = 1; i <= N; i++) {
-    logs.push([i - 1, ...input[i].split(' ').map(elem => elem - 1)]);
+    logs.push([i - 1, ...input[i].split(' ').map(Number)]);
 }
 
 const parents = Array.from(Array(N), (_, k) => k);
-const find = a => {
-    if (a === parents[a]) {
-        return a;
-    }
-
-    return parents[a] = find(parents[a])
-}
-
-const union = (a, b) => {
-    a = find(a);
-    b = find(b);
-    if (a === b) {
-        return;
-    }
-
-    parents[Math.max(a, b)] = Math.min(a, b);
-}
 
 logs.sort((a, b) => {
     const [c1, ax1, ax2, ay] = a;
     const [c2, bx1, bx2, by] = b;
 
-    if (ax1 === bx1) {
-        if (ax2 === bx2) {
-            return ay - by;
-        }
-  
-        return ax2 - bx2;
-    }
-
+    
     return ax1 - bx1;
 })
 
-// console.log(logs)
-const canJump = (a, b) => {
-    const [c1, ax1, ax2, ay] = a;
-    const [c2, bx1, bx2, by] = b;
 
-
-    return ax2 >= bx1;
-}
-
+let maxX = logs[0][2];
 
 for (let i = 0; i < N - 1; i++) {
     const prev = logs[i];
     const next = logs[i + 1];
 
-    if (find(prev[0]) === find[next[0]]) {
+    if (parents[prev[0]] === parents[next[0]]) {
         continue;
     }
 
-    if (canJump(prev, next)) {
-        union(prev[0], next[0]);
+    maxX = Math.max(maxX, prev[2]);
+
+    if (maxX >= next[1]) {
+        parents[next[0]] = parents[prev[0]];
+    } else {
+        maxX = next[2];
     }
 }
 
@@ -75,7 +48,7 @@ const answer = [];
 for (let i = N + 1; i < input.length; i++) {
     const [s, e] = input[i].split(' ').map(elem => elem - 1);
 
-    if (find(s) === find(e)) {
+    if (parents[s] === parents[e]) {
         answer.push(1);
     } else {
         answer.push(0);
