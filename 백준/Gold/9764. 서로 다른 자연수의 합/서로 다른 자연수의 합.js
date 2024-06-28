af2c496fb6f5a14874468e14bd7987e6ca1b5044
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-06-28 21:39:52 +0900

    [Gold V] Title: 서로 다른 자연수의 합, Time: 344 ms, Memory: 73828 KB -BaekjoonHub

--- 백준/Gold/9764. 서로 다른 자연수의 합/서로 다른 자연수의 합.js
+++ 백준/Gold/9764. 서로 다른 자연수의 합/서로 다른 자연수의 합.js
@@ -9,23 +9,18 @@ const [N, ...testCases] = require('fs')
 const max = Math.max(...testCases);
 const dp = Array.from(Array(max + 1), () => Array(max + 1).fill(0));
 
-for (let i = 1 ; i <= max; i++) {
-  dp[i][i] = 1;
-}
+dp[0][0] = 1;
 
+for (let i = 0; i <= max; i++) {
+  for (let j = 1; j <= max; j++) {
+    dp[i][j] = dp[i][j - 1];
 
-
-for (let i = 2; i <= max; i++) {
-  for (let j = 1; j < i; j++) {
-    dp[i][j] = 0;
-    for (let k = j + 1; k<= i; k++) {
-      dp[i][j] += dp[i - j][k];
-      dp[i][j] %= 100999;
+    if (j > i) {
+      continue;
     }
+
+    dp[i][j] = (dp[i][j] + dp[i - j][j - 1]) % 100999
   }
 }
 
-const dp2 = dp.map(elem => elem.reduce((pv, cv) => (pv + cv) % 100999, 0));
-
-
-testCases.forEach(elem => console.log(dp2[elem] % 100999));
+console.log(testCases.map(elem => dp[elem][elem] % 100999).join('\n'))
