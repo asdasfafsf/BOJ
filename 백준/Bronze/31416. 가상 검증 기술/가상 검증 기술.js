a6d25a4cfc66078079d234164cdd1e13540cad3b
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2024-09-10 19:44:49 +0900

    [Bronze I] Title: 가상 검증 기술, Time: 88 ms, Memory: 10132 KB -BaekjoonHub

--- 백준/Bronze/31416. 가상 검증 기술/가상 검증 기술.js
+++ 백준/Bronze/31416. 가상 검증 기술/가상 검증 기술.js
@@ -5,6 +5,7 @@ const input = require('fs')
     .split('\n')
 
 const Q = +input[0];
+const answer = [];
 
 for (let i = 1; i <= Q; i++) {
     const [TA, TB, VA, VB] = input[i].split(' ').map(Number)
@@ -12,21 +13,13 @@ for (let i = 1; i <= Q; i++) {
     let 걸린시간 = VB * TB;
 
     const A가남은갯수 = VA - Math.floor(걸린시간 / TA);
-    // console.log(`A가남은갯수 : ${A가남은갯수}`)
-    // console.log(`현재걸린시간 : ${걸린시간}`)
 
     if (A가남은갯수 % 2 === 1) {
-        const A가마지막작업이면서남은시간 = 걸린시간 >= TA 
-            ? TA - (걸린시간 % TA)
-            : (TA - 걸린시간);
-        
-        // console.log(걸린시간 > TA )
-        // console.log(`A가마지막작업이면서남은시간 : ${A가마지막작업이면서남은시간}`)
-        걸린시간 += A가마지막작업이면서남은시간;
+        걸린시간 += TA - (걸린시간 % TA);
     }
     걸린시간 += Math.floor(Math.max(A가남은갯수, 0) / 2) * TA;
-    console.log(걸린시간)
-    // break;
+    answer.push(걸린시간)
 }
 
 
+console.log(answer.join('\n'))
