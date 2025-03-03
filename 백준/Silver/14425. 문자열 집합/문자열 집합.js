f652f4644047ec23f22386bbfe603f9d579e1ec1
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-03-03 16:44:48 +0900

    [Silver IV] Title: 문자열 집합, Time: 144 ms, Memory: 31808 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -1,18 +1,17 @@
 const input = require('fs')
-    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
-    .toString()
-    .trim()
-    .split('\n');
-
+.readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+.toString()
+.trim()
+.split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
 
-const map = Object.fromEntries(input.slice(1, N + 1).map(elem =>[elem, true]));
+const set = new Set(input.slice(1, N + 1));
 
 let answer = 0;
 for (let i = N + 1; i < input.length; i++) {
-    if (map[input[i]]) {
+    if (set.has(input[i])) {
         answer++;
     }
 }
-console.log(answer)
+console.log(answer);
