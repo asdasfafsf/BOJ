c3912d5cdf8e0043752646540069eaaffb890ef6
Wonkeun <asdasfafsf@naver.com>
2025-06-14 15:49:16 +0900

    [Bronze V] Title: 기찍 N, Time: 184 ms, Memory: 24224 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.js
+++ 백준/Bronze/2742. 기찍 N/기찍 N.js
@@ -1,5 +1,7 @@
 const input = +require('fs').readFileSync(0, 'utf-8');
 
+const answer = []
 for (let i = input; i >= 1; i--) {
-  console.log(i);
+  answer.push(i);
 }
+console.log(answer.join('\n'));
