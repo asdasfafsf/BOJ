c01cc659e56a9e9bc159e07f45b3776be1e8fa46
Wonkeun <asdasfafsf@naver.com>
2025-08-26 20:00:15 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 88 ms, Memory: 9424 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.js
@@ -0,0 +1,9 @@
+const input = require('fs')
+    .readFileSync(0, 'utf-8')
+    .toString()
+    .trim()
+    .split('\n')
+
+const N = Number(input[0])
+const S = input[1]
+console.log(S.slice(-5))
