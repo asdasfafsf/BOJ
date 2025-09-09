4bd6339d4e506771fc1a1c69cf3dfeed471482c7
Wonkeun <asdasfafsf@naver.com>
2025-09-10 06:31:10 +0900

    [Bronze IV] Title: Silnia, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/8558. Silnia/Silnia.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync(0, 'utf-8').trim()
+const n = Number(input)
+
+if (n === 0) {
+    console.log(1)
+} else if (n === 1) {
+    console.log(1)
+} else if (n === 2) {
+    console.log(2)
+} else if (n === 3) {
+    console.log(6)
+} else if (n === 4) {
+    console.log(4)
+} else {
+    console.log(0)
+}
