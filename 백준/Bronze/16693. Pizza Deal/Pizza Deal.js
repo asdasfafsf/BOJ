6fa51e787a2ebc211a0d1db9edb83dc2086f4dee
Wonkeun <asdasfafsf@naver.com>
2025-10-26 11:32:38 +0900

    [Bronze IV] Title: Pizza Deal, Time: 84 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/16693. Pizza Deal/Pizza Deal.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n')
+const [A1, P1] = input[0].split(' ').map(Number)
+const [R1, P2] = input[1].split(' ').map(Number)
+
+const sliceValue = A1 / P1
+const wholeValue = (Math.PI * R1 * R1) / P2
+
+if (sliceValue > wholeValue) console.log('Slice of pizza')
+else console.log('Whole pizza')
